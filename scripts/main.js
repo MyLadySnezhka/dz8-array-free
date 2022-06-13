397c433446c37b94201130eca4339d344e242b9b
MyLadySnezhka <mylady.snezhka@gmail.com>
2022-06-13 03:25:10 +0300

    add arr for each

--- scripts/main.js
+++ scripts/main.js
@@ -2,14 +2,49 @@ const itemlink = document.querySelector('input[name="itemname"]');
 const butlink = document.querySelector('button[name="enter"]');
 const spisoklink = document.querySelector('.spisok');
 
-let itemnazv = '';
+let itemnazv;
 const magaz = [];
+let html = '';
+
+    
+    butlink.addEventListener('click', (ev) => {
+        spisoklink.innerHTML = '';
+        itemnazv = itemlink.value;
+        magaz.push(itemnazv);
+        //console.log(magaz);      
+        itemlink.value = '';
+        zapis();
+        
+    });
+
+
+    const zapis = () => {
+        
+        magaz.forEach((itemnazv) => {
+           html = html + '<li>' + itemnazv + '</li>';  
+        })
+        spisoklink.innerHTML = html;        
+    }
+
+    
+
+
+    //ВОТ ТАК ШИКАРНО РАБОТАЕТ!!!!
+    
+    /*
+
+const itemlink = document.querySelector('input[name="itemname"]');
+const butlink = document.querySelector('button[name="enter"]');
+const spisoklink = document.querySelector('.spisok');
+
+let itemnazv = '';
+//const magaz = [];
 let html = '';
     
     butlink.addEventListener('click', (ev) => {
         
         itemnazv = itemlink.value;
-        magaz.push({itemnazv});
+        // magaz.push({itemnazv});
         // console.log(magaz);
         
 
@@ -19,5 +54,4 @@ let html = '';
         itemlink.value = '';
     });
 
-
-    
+    */
